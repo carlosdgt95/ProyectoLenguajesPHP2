b372d467ec67828f212bce25e5419d242d648521
Karla Castro <kycastro@espol.edu.ec>
2023-01-15 23:11:19 -0500

    Fix data parser on array

--- get.php
+++ get.php
@@ -17,12 +17,16 @@ $data= json_decode($response, true);
 $contadorid=0;
 $arrayjson1=[];
 foreach($data as $key => $value){
-    // echo "nombre:" ,$data[$key]["nombre"]."<br>";
-    // echo "imagen:" ,$data[$key]["imagen"]."<br>";
-    // echo "descripcion:" ,$data[$key]["descripcion"]."<br>";
-  $arrayjson=["nombre"=>$data[$key]["nombre"]
-    ,"imagen"=>$data[$key]["imagen"]
-   ,"descripcion"=>$data[$key]["descripcion"]];
+  // echo "nombre:" ,$data[$key]["nombre"]."<br>";
+  // echo "imagen:" ,$data[$key]["imagen"]."<br>";
+  // echo "descripcion:" ,$data[$key]["descripcion"]."<br>";
+    $arrayjson=["actividades"=>$data[$key]["actividades"]
+      ,"capacidad"=>$data[$key]["capacidad"]
+    ,"horario"=>$data[$key]["horario"]
+    ,"nombre"=>$data[$key]["nombre"]
+    ,"website"=>$data[$key]["website"]
+
+    ];
     
    print_r(json_encode($arrayjson));
 
